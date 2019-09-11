7248e99f33919866a8f4ecfaaa8b8957f1f4dbd2
Dawit <dawit@ginetta.net>
2019-09-11 11:18:54 +0200

    Store variables for useState

--- src/components/filtered-list.js
+++ src/components/filtered-list.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const FilteredList = () => {
+const FilteredList = (props) => {
+	const [ allItems, setAllItems ] = useState('');
+
 	return <div />;
 };
 
