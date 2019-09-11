e913d8e648fdc708b541c20dc557177bd66a1021
Dawit <dawit@ginetta.net>
2019-09-11 10:32:27 +0200

    Add .js extension to the file, create a search component and populate the the functions by importing react, defining the function and default exporting it

---
+++ src/components/list-component.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const ListComponent = () => {
+	return (
+		<div>
+			<ul />
+		</div>
+	);
+};
+
+export default ListComponent;
---
+++ src/components/search-component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchComponent = () => {
+	return (
+		<form>
+			<label htmlFor="input" />
+			<input type="text" id="input">
+				Search
+			</input>
+		</form>
+	);
+};
+
+export default SearchComponent;
